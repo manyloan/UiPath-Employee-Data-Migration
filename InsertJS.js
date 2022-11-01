8566c3a57d54e3279b5138ae29ad5a3d25003e92
Haniel Loan <haniel.loan@gmail.com>
2022-10-31 22:13:11 -0300

    Add files via upload

---
+++ InsertJS.js
@@ -0,0 +1,14 @@
+function(element){
+    document.getElementById("firstName").value = "FIRSTNAME";
+    document.getElementById("lastName").value = "LASTNAME";
+    document.getElementById("phone").value = "PHONE";
+    document.getElementById("email").value = "EMAIL";
+    document.getElementById("city").value = "CITY";
+    document.getElementById("state").value = "STATE";
+    document.getElementById("zip").value = "ZIP";
+    document.getElementById("title").value = "JOBTITLE";
+    document.getElementById("department").value = "DEPARTMENT";
+    document.getElementById("startDate").value = "STARTDATE";
+    document.getElementById("manager").value = "MANAGER";
+    //SubmitClick
+}
